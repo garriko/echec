8bc4d87b2250d5a10aea2d1094722edfb8b7ea22
alroad <alroad1234@hotmail.fr>
2012-12-17 17:42:19 +0100

    pour le plaisir

--- echec/src/test/java/projet_echec/echec/jeu/PieceTest.java
+++ echec/src/test/java/projet_echec/echec/jeu/PieceTest.java
@@ -1,10 +1,4 @@
 package projet_echec.echec.jeu;
-import projet_echec.echec.jeu.piece.Cavalier;
-import projet_echec.echec.jeu.piece.Fou;
-import projet_echec.echec.jeu.piece.Pion;
-import projet_echec.echec.jeu.piece.Reine;
-import projet_echec.echec.jeu.piece.Roi;
-import projet_echec.echec.jeu.piece.Tour;
 import junit.framework.TestCase;
 
 
