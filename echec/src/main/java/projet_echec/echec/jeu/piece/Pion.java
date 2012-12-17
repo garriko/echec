436da7045201da38c9697c7540f95919393f8b23
Kevin <kevg93@hotmail.fr>
2012-12-17 17:24:00 +0100

    deplacement pion

--- echec/src/main/java/projet_echec/echec/jeu/piece/Pion.java
+++ echec/src/main/java/projet_echec/echec/jeu/piece/Pion.java
@@ -3,6 +3,7 @@ package projet_echec.echec.jeu.piece;
 import java.util.ArrayList;
 import projet_echec.echec.jeu.Case;
 import projet_echec.echec.jeu.Piece;
+import projet_echec.echec.jeu.Position;
 
 
 /**
@@ -25,9 +26,30 @@ public class Pion extends Piece {
 	
 	
 	@Override
-	public ArrayList<Case> getDeplacementPossible(Case caseActuelle) {
+	public ArrayList<Case> getDeplacementPossible(Case pos){return null;}
+	
+	public ArrayList<Case> getDeplacementPossible(Case pos, ArrayList<Case> caseDiagonale) {
 		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Case> lp = new ArrayList<Case>();
+		if(dejaBouge == false)
+		{
+			Case c0 = new Case(new Position(pos.getPosition().getHauteur()+1,pos.getPosition().getLargeur()+0));
+			if(isDeplacementOk(c0))
+				lp.add(c0);
+			
+			Case c1 = new Case(new Position(pos.getPosition().getHauteur()+2,pos.getPosition().getLargeur()+0));
+			if(isDeplacementOk(c1))
+				lp.add(c1);
+			dejaBouge = true;
+		}
+		else
+		{
+			Case c0 = new Case(new Position(pos.getPosition().getHauteur()+1,pos.getPosition().getLargeur()+0));
+			if(isDeplacementOk(c0))
+				lp.add(c0);
+		}
+		lp.addAll(caseDiagonale);
+		return lp;
 	}
 
 	/**
