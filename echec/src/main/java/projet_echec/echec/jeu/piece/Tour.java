3b2b092286b66971e65a7f83968c0a09b63a8ef4
alroad <alroad1234@hotmail.fr>
2012-12-17 16:02:09 +0100

    class fou et tour: getdeplacement possible()

--- echec/src/main/java/projet_echec/echec/jeu/piece/Tour.java
+++ echec/src/main/java/projet_echec/echec/jeu/piece/Tour.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import projet_echec.echec.jeu.Case;
 import projet_echec.echec.jeu.Piece;
+import projet_echec.echec.jeu.Position;
 
 /**
  * 
@@ -22,9 +23,34 @@ public class Tour extends Piece{
 	}
 
 	@Override
-	public ArrayList<Case> getDeplacementPossible(Case caseActuelle) {
-		// TODO Auto-generated method stub
-		return null;
+	public ArrayList<Case> getDeplacementPossible(Case pos) {
+			ArrayList<Case> lp = new ArrayList<Case>();
+			Case h = new Case(new Position(pos.getPosition().getHauteur(),pos.getPosition().getLargeur()));
+			Case d = new Case(new Position(pos.getPosition().getHauteur(),pos.getPosition().getLargeur()));
+			Case b = new Case(new Position(pos.getPosition().getHauteur(),pos.getPosition().getLargeur()));
+			Case g = new Case(new Position(pos.getPosition().getHauteur(),pos.getPosition().getLargeur()));
+			
+			for (int i=0;i<8;i++)
+			{
+				h.setPosition(new Position(h.getPosition().getHauteur()+1,h.getPosition().getLargeur()));
+				d.setPosition(new Position(d.getPosition().getHauteur(),d.getPosition().getLargeur()+1));
+				b.setPosition(new Position(b.getPosition().getHauteur()-1,b.getPosition().getLargeur()));
+				g.setPosition(new Position(g.getPosition().getHauteur(),g.getPosition().getLargeur()-1));
+				
+				if(isDeplacementOk(h)){
+					lp.add(h);}
+					
+				if(isDeplacementOk(d)){
+					lp.add(d);}
+				
+				if(isDeplacementOk(b)){
+					lp.add(b);}
+				
+				if(isDeplacementOk(g)){
+					lp.add(g);}
+			}
+			return lp;
+			}
 	}
 
-}
+
