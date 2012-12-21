5152ef34e7d12fef23ec19b5286176f4299b04fa
Anne-Sophie <annesotir@free.fr>
2012-12-21 10:12:16 +0100

    revoir partie debut

--- echec/src/main/java/interfaceGraphique/InterfaceJeu.java
+++ echec/src/main/java/interfaceGraphique/InterfaceJeu.java
@@ -711,34 +711,8 @@ public class InterfaceJeu {
 				
 				if (getTour()==1){
 					if (eCase.estVide()==false){
-						if (eCase.getPiece().getCamp().equals("blanc")){
-							
-							
-							ArrayList<Case> plop = new ArrayList<Case>();
-							boolean depEffectue = false;
-							plop = eCase.getPiece().getDeplacementPossible(eCase);//donne les déplacements possible de la piece présent sur la case depart
-							
-							if(eCase.getPiece().getClass().getSimpleName().equals(new String("Fou"))||eCase.getPiece().getClass().getSimpleName().equals(new String("Reine"))){
-								plateauJeu.filtrePourFou(eCase, plop);
-								System.out.println("pas bon 1");
-							}
-							
-							if(eCase.getPiece().getClass().getSimpleName().equals(new String("Tour"))||eCase.getPiece().getClass().getSimpleName().equals(new String("Reine"))){
-								plateauJeu.filtrePourTour(eCase, plop);
-								System.out.println("pas bon 2");
-							}
-							
-							plop = plateauJeu.filtrerDeplacementPossible(eCase.getPiece().getCamp(), plop);//filtre si il n'y pas de pieces
-
-							
-							System.out.println("filtre 1");
-							if(eCase.getPiece().getClass().getSimpleName().equals(new String("Pion"))){
-								plateauJeu.filtrerpresenceAdversaireDiagonale(eCase,plop);
-								System.out.println("pas bon 3");
-							}
-							
-							
-											
+						if (eCase.getPiece().getCamp().equals("blanc")){							
+							ArrayList<Case> plop = plateauJeu.deplacementPossible(eCase);	
 							for (int i=0; i<plop.size(); i++){
 								Case laCase = plop.get(i);
 								Position po = new Position(8-laCase.getPosition().getHauteur()+1, laCase.getPosition().getLargeur());
@@ -753,31 +727,7 @@ public class InterfaceJeu {
 				else if (getTour()==2){
 					if (eCase.estVide()==false){
 						if (eCase.getPiece().getCamp().equals("noir")){
-							ArrayList<Case> plop = new ArrayList<Case>();
-							boolean depEffectue = false;
-							plop = eCase.getPiece().getDeplacementPossible(eCase);//donne les déplacements possible de la piece présent sur la case depart
-							
-							if(eCase.getPiece().getClass().getSimpleName().equals(new String("Fou"))||eCase.getPiece().getClass().getSimpleName().equals(new String("Reine"))){
-								plateauJeu.filtrePourFou(eCase, plop);
-								System.out.println("pas bon 1");
-							}
-							
-							if(eCase.getPiece().getClass().getSimpleName().equals(new String("Tour"))||eCase.getPiece().getClass().getSimpleName().equals(new String("Reine"))){
-								plateauJeu.filtrePourTour(eCase, plop);
-								System.out.println("pas bon 2");
-							}
-							
-							plop = plateauJeu.filtrerDeplacementPossible(eCase.getPiece().getCamp(), plop);//filtre si il n'y pas de pieces
-
-							
-							System.out.println("filtre 1");
-							if(eCase.getPiece().getClass().getSimpleName().equals(new String("Pion"))){
-								plateauJeu.filtrerpresenceAdversaireDiagonale(eCase,plop);
-								System.out.println("pas bon 3");
-							}
-							
-														
-							
+							ArrayList<Case> plop = plateauJeu.deplacementPossible(eCase);					
 							for (int i=0; i<plop.size(); i++){
 								Case laCase = plop.get(i);
 								Position po = new Position(8-laCase.getPosition().getHauteur()+1, laCase.getPosition().getLargeur());
@@ -799,40 +749,14 @@ public class InterfaceJeu {
 			Case eCase = plateauJeu.chercherCase(p);
 			if (getTour()==1){
 				if (eCase.estVide()==false){
-					if (eCase.getPiece().getCamp().equals("blanc")){
-						
-						
-						ArrayList<Case> plop = new ArrayList<Case>();
-						boolean depEffectue = false;
-						plop = eCase.getPiece().getDeplacementPossible(eCase);//donne les déplacements possible de la piece présent sur la case depart
-						
-						if(eCase.getPiece().getClass().getSimpleName().equals(new String("Fou"))||eCase.getPiece().getClass().getSimpleName().equals(new String("Reine"))){
-							plateauJeu.filtrePourFou(eCase, plop);
-							System.out.println("pas bon 1");
-						}
-						
-						if(eCase.getPiece().getClass().getSimpleName().equals(new String("Tour"))||eCase.getPiece().getClass().getSimpleName().equals(new String("Reine"))){
-							plateauJeu.filtrePourTour(eCase, plop);
-							System.out.println("pas bon 2");
-						}
-						
-						plop = plateauJeu.filtrerDeplacementPossible(eCase.getPiece().getCamp(), plop);//filtre si il n'y pas de pieces
-
-						
-						System.out.println("filtre 1");
-						if(eCase.getPiece().getClass().getSimpleName().equals(new String("Pion"))){
-							plateauJeu.filtrerpresenceAdversaireDiagonale(eCase,plop);
-							System.out.println("pas bon 3");
-						}
-						
-						
-										
-						for (int i=0; i<plop.size(); i++){
+					if (eCase.getPiece().getCamp().equals("blanc")){					
+							ArrayList<Case> plop = plateauJeu.deplacementPossible(eCase);
+							for (int i=0; i<plop.size(); i++){
 							Case laCase = plop.get(i);
 							Position po = new Position(8-laCase.getPosition().getHauteur()+1, laCase.getPosition().getLargeur());
 							int num = po.getLargeur() + 8*(po.getHauteur()-1)-1;		
 							((Vector<JButton>) tab_cases).get(num).setBorderPainted(true);
-							((Vector<JButton>) tab_cases).get(num).setBorder(BorderFactory.createLineBorder(Color.blue));
+							((Vector<JButton>) tab_cases).get(num).setBorder(BorderFactory.createLineBorder(Color.gray));
 						}
 					}
 				}
@@ -841,43 +765,18 @@ public class InterfaceJeu {
 			else if (getTour()==2){
 				if (eCase.estVide()==false){
 					if (eCase.getPiece().getCamp().equals("noir")){
-						ArrayList<Case> plop = new ArrayList<Case>();
-						boolean depEffectue = false;
-						plop = eCase.getPiece().getDeplacementPossible(eCase);//donne les déplacements possible de la piece présent sur la case depart
-						
-						if(eCase.getPiece().getClass().getSimpleName().equals(new String("Fou"))||eCase.getPiece().getClass().getSimpleName().equals(new String("Reine"))){
-							plateauJeu.filtrePourFou(eCase, plop);
-							System.out.println("pas bon 1");
-						}
-						
-						if(eCase.getPiece().getClass().getSimpleName().equals(new String("Tour"))||eCase.getPiece().getClass().getSimpleName().equals(new String("Reine"))){
-							plateauJeu.filtrePourTour(eCase, plop);
-							System.out.println("pas bon 2");
-						}
-						
-						plop = plateauJeu.filtrerDeplacementPossible(eCase.getPiece().getCamp(), plop);//filtre si il n'y pas de pieces
-
-						
-						System.out.println("filtre 1");
-						if(eCase.getPiece().getClass().getSimpleName().equals(new String("Pion"))){
-							plateauJeu.filtrerpresenceAdversaireDiagonale(eCase,plop);
-							System.out.println("pas bon 3");
-						}
-						
-													
-						
+						ArrayList<Case> plop = plateauJeu.deplacementPossible(eCase);			
 						for (int i=0; i<plop.size(); i++){
 							Case laCase = plop.get(i);
 							Position po = new Position(8-laCase.getPosition().getHauteur()+1, laCase.getPosition().getLargeur());
 							int num = po.getLargeur() + 8*(po.getHauteur()-1)-1;		
 							((Vector<JButton>) tab_cases).get(num).setBorderPainted(true);
-							((Vector<JButton>) tab_cases).get(num).setBorder(BorderFactory.createLineBorder(Color.blue));
+							((Vector<JButton>) tab_cases).get(num).setBorder(BorderFactory.createLineBorder(Color.gray));
 						}
 					}
 				}
 			}
-		}
-		}
+		}		
 	}
 	
 
--- echec/src/main/java/interfaceGraphique/InterfaceRevoirPartie.java
+++ echec/src/main/java/interfaceGraphique/InterfaceRevoirPartie.java
@@ -1,13 +1,18 @@
 package interfaceGraphique;
 
 import interfaceGraphique.InterfaceConfigPartie.Ecouteur;
+import interfaceGraphique.InterfaceJeu.CountdownTimerListener;
 
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Vector;
 
 import javax.swing.ComboBoxModel;
 import javax.swing.ImageIcon;
@@ -21,15 +26,19 @@ import javax.swing.JSpinner;
 import javax.swing.JTextField;
 import javax.swing.SpinnerListModel;
 import javax.swing.SpinnerNumberModel;
+import javax.swing.Timer;
 
 import projet_echec.echec.gestion.Partie;
 import projet_echec.echec.gestion.SaveGame;
+import projet_echec.echec.jeu.Case;
+import projet_echec.echec.jeu.EchiquierActif;
+import projet_echec.echec.jeu.Position;
 import projet_echec.echec.wrapper.Wrapper;
 
 
 /**
  * 
- * @author Vincent
+ * @author Anne-Sophie
  *
  * Fênetre lors du replay d'une partie
  *
@@ -48,7 +57,14 @@ public class InterfaceRevoirPartie {
 	JSpinner cadence ;
 	JComboBox modeLecture;
 	
-	JLabel mode;
+	Collection<JLabel> tab_cases;
+	
+	Wrapper w;
+	Partie p;
+	EchiquierActif plateauJeu;
+	
+	Timer countdown; 
+	int timeRemaining;
 	
 	
 	/**
@@ -60,7 +76,7 @@ public class InterfaceRevoirPartie {
 		fenetre.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		
 		try {
-			Wrapper w = SaveGame.charger(nomPartie);
+			w = SaveGame.charger(nomPartie);
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -68,6 +84,8 @@ public class InterfaceRevoirPartie {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		p = w.getP();
+		plateauJeu = (EchiquierActif) w.getE();
 		
 		
 		boutonLect = new ImageIcon("images/interface_revoirpartieBoutonLecture.png").getImage().getScaledInstance(92, 64, Image.SCALE_DEFAULT);
@@ -79,7 +97,6 @@ public class InterfaceRevoirPartie {
 		cadence = new JSpinner(new SpinnerNumberModel(cad, cad - 0, cad + 50,1));
 		modeLecture = new JComboBox();
 		
-		mode = new JLabel();
 		
 		BoutonLect.setBounds(85, 590, 92, 64); //position x, position y, largeur, hauteur
 		BoutonPause.setBounds(180, 590, 92, 65); 
@@ -92,34 +109,148 @@ public class InterfaceRevoirPartie {
 		BoutonLect.addActionListener(listen);
 		BoutonPause.addActionListener(listen);
 		
-	
-		JPanel boutonsChoix = new TestImagePanel(new ImageIcon("images/interfaceRevoirPartie.png").getImage().getScaledInstance(1035, 700, Image.SCALE_DEFAULT));
-		boutonsChoix.setLayout(null);
 		
-		boutonsChoix.add(BoutonLect);   		
-		boutonsChoix.add(BoutonPause);
-		boutonsChoix.add(mode);
-		boutonsChoix.add(cadence);
-		boutonsChoix.add(modeLecture);
+		// Initialisation de l'échiquier
+		tab_cases = new Vector<JLabel>	();	
+		
+		for (int i=0; i<8; i++){
+			for (int j=0; j<8; j++){
+				
+				if (j%2==0){
+					
+					if (i%2==0){
+						tab_cases.add(new JLabel(new ImageIcon("images/caseblanche.png")));
+					}
+					else {
+						tab_cases.add(new JLabel(new ImageIcon("images/casegrise.png")));
+					}
+				}
+				else {
+					if (i%2==1){
+						tab_cases.add(new JLabel(new ImageIcon("images/caseblanche.png")));
+					}
+					else {
+						tab_cases.add(new JLabel(new ImageIcon("images/casegrise.png")));
+					}			
+				}
+				((Vector<JLabel>) tab_cases).get(i).setPreferredSize(new Dimension(45,45));
+			}
+		}
+		
+		
+		
+		
+		JPanel plateau = new JPanel();
+		plateau.setLayout(new GridLayout(8,8));
+	
+		plateau.setBounds(505,90,360,360);		
+		
+		
+		for (int i=0; i<64 ; i++){
+			plateau.add(((Vector<JLabel>) tab_cases).get(i));
+			//((Vector<JButton>) tab_cases).get(i).setBorder(BorderFactory.createLineBorder(Color.gray));
+		}		
+		
+		
+		
+		JPanel imageFond = new TestImagePanel(new ImageIcon("images/interfaceRevoirPartie.png").getImage().getScaledInstance(1035, 700, Image.SCALE_DEFAULT));
+		imageFond.setLayout(null);
+		
+		imageFond.add(plateau);
+		
+		
+		timeRemaining = (Integer) cadence.getValue(); 		
+		countdown = new Timer(1000, new CountdownTimerListener()); // On crée le timer
+		countdown.start(); //Démarrer le compteur
+	
+		
+		imageFond.add(BoutonLect);   		
+		imageFond.add(BoutonPause);
+		imageFond.add(cadence);
+		imageFond.add(modeLecture);
 		
 		modeLecture.addItem("Continue");
 	    modeLecture.addItem("Pas à pas");
 
 		
 		
-		boutonsChoix.setOpaque(false);
-		tmp.add(boutonsChoix);
+		imageFond.setOpaque(false);
+		tmp.add(imageFond);
     
 		fenetre.setSize(1035,720); // taille de l'image de fond
 		fenetre.setResizable(false);
 		fenetre.setVisible(true);
 	}
 	
+	
+	
+	/**
+	 * Methode permettant de mettre a jour l'image d'une case
+	 * 
+	 * @param NewCase la case dont l'image doit etre rechargee
+	 */
+	
+	public void actualiserImage(Case NewCase){
+		Position p = new Position(8-NewCase.getPosition().getHauteur()+1, NewCase.getPosition().getLargeur());
+		int numCase = p.getLargeur() + 8*(p.getHauteur()-1)-1;	
+		((Vector<JLabel>) tab_cases).get(numCase).setIcon(new ImageIcon(NewCase.getImg()));
+	}
+	
+	
+	public void deplacementSuivant(){
+		//case départ
+		//case arrivee
+		//actualiserImage
+	}
+	
+	public void play(){
+		
+	}
+	
+	public void pause(){
+		
+	}
+	
 		
 	public class Ecouteur implements ActionListener{		
 		public void actionPerformed(ActionEvent e){
+			if (((String) modeLecture.getSelectedItem()).equals(new String("Pas à pas"))){	
+				if (e.getSource()==BoutonLect){
+					deplacementSuivant();
+				}
+			}
+			else {
+				if (e.getSource()==BoutonLect){
+					play();
+				}
+				if (e.getSource()==BoutonPause){
+					pause();
+				}
+			}
 		}
 	}
+	
+	
+	
+	class CountdownTimerListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			
+			if (--timeRemaining >= 0) {
+				countdown.start();
+			} 
+			else {
+				countdown.stop();
+				deplacementSuivant();
+				timeRemaining = (Integer) cadence.getValue();
+				countdown.start();
+			}
+			
+				
+			
+		}
+	}
+	
+	
 	
 	public static void main(String[] args){
 		//new InterfaceRevoirPartie();
